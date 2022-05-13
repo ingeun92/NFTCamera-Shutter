92da89134de093d29b265fa07a7c6d99ab2a2524
Ingeun Kim <ingeun92@naver.com>
2022-05-13 16:32:38 +0900

    set .prettierrc

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Define a route handler for the default home page
-app.get('/', (req: any, res: any) => {
-  res.send('Hello world!');
+app.get("/", (req: any, res: any) => {
+  res.send("Hello world!");
 });
 
-app.get('/getNFTList/:userAddress', (req: any, res: any) => {
-  res.send('NFT List');
+app.get("/getNFTList/:userAddress", (req: any, res: any) => {
+  res.send("NFT List");
 });
 
-app.get('/getMediaURI/:tokenID', (req: any, res: any) => {
-  res.send('Media URI');
+app.get("/getMediaURI/:tokenID", (req: any, res: any) => {
+  res.send("Media URI");
 });
 
-app.get('/getMetadata/:tokenID', (req: any, res: any) => {
-  res.send('Metadata');
+app.get("/getMetadata/:tokenID", (req: any, res: any) => {
+  res.send("Metadata");
 });
 
-app.post('/mintNFT/:metadata', (req: any, res: any) => {
-  res.send('Mint NFT');
+app.post("/mintNFT/:metadata", (req: any, res: any) => {
+  res.send("Mint NFT");
 });
 
 // Start the Express server
