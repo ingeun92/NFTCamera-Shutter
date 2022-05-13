8d80b5ba71e8db591d8745cced969e23f0f61bc3
magnesae <opaduel@hotmail.com>
2022-05-13 15:29:48 +0900

    First commit, added structure

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Define a route handler for the default home page
+app.get('/', (req: any, res: any) => {
+  res.send('Hello world!');
+});
+
+app.get('/getNFTList/:userAddress', (req: any, res: any) => {
+  res.send('NFT List');
+});
+
+app.get('/getMediaURI/:tokenID', (req: any, res: any) => {
+  res.send('Media URI');
+});
+
+app.get('/getMetadata/:tokenID', (req: any, res: any) => {
+  res.send('Metadata');
+});
+
+app.post('/mintNFT/:metadata', (req: any, res: any) => {
+  res.send('Mint NFT');
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
