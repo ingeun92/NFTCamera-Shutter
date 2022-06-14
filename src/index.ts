67c3fdf21a6570992cddd4dbc7f600e0ec8b90f2
magnesae <opaduel@hotmail.com>
2022-06-14 14:47:59 +0900

    added cors options

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 
 app.use("/logchain", logchainRoutes);
 app.use("/besu", besuRoutes);
